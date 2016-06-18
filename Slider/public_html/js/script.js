8cbf601f7e24d144ad9431bab89a99bc59d5499b
PRABHAKAR <PRABHAKAR@DESKTOP-R0SNNFJ.local.lan>
2016-06-18 22:31:36 +0530

    Initial

---
+++ Slider/public_html/js/script.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function (){
+  
+//$(".slider").bxSlider();    
+   $.fn.abc = function(){
+
+		if(this.length == 0) return this;
+
+		// support mutltiple elements
+		if(this.length > 0){
+                    $(this).find("li").addClass("slider_li");
+                    $(this).find(".slider_li:first").addClass("active");
+                    debugger;
+                    $(this).find(".active").fadeIn().delay(1200).fadeOut().removeClass("active").next().addClass("active");
+                    re_call();
+		}
+            };
+            $(".slider").abc();
+            function re_call(){
+                $(".active").fadeIn().delay(1200).fadeOut().removeClass("active").next().addClass("active");
+                
+            }
+            
+});
