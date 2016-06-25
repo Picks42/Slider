65f539a14a73e48cec950eea7eebd2e4e0bf4b63
PRABHAKAR <PRABHAKAR@DESKTOP-R0SNNFJ.local.lan>
2016-06-25 12:06:45 +0530

    Initial

--- Slider/public_html/js/script.js
+++ Slider/public_html/js/script.js
@@ -9,36 +9,76 @@ $(document).ready(function () {
 
 //$(".slider").bxSlider();   
 
-    $.fn.abc = function () {
-        var that=this;
-        var reset = function () {
-            $(that).find(".slider_li:first").addClass("active");
-
-            $(that).find(".active").fadeIn().delay(1200).fadeOut().removeClass("active").next().addClass("active");
-            re_call();
-        }
-        var re_call = function () {
-            setInterval(function () {
-                $(that).find(".active").fadeIn().delay(1200).fadeOut().removeClass("active").next().addClass("active");
-                if ($(".active").next().length == 0) {
-                    $(that).find(".active").removeClass(".active");
-                    reset();
-                }
-            }, 3000);
-        }
-
-        //slider call
-        if (this.length == 0)
-            return this;
-        // support mutltiple elements
-        if (this.length > 0) {
+    $.fn.picksSilder = function () {
+        var that = this;
+        
+        var total_slide;
+        var child;
+        child=$(that).children();
+        var count;
+         count=child.length;
+      var init = function () {
+            $(that).addClass("picksSlider").attr("data-current","slide-0");
+             
+            
+             total_slide=parseInt(count)-1;
+            $.each(child,function (index,item){
+               $(item).addClass("item slide-"+index); 
+            });
+        };
+        var min_height= function (){
+            $(that).wrap("<div class='Slider_container'></div>");
             debugger;
-            $(that).find("li").addClass("slider_li");
-            reset();
-        }
-
+             var min=0;
+             var current;
+            $.each(child,function (){
+              
+               current=$(this).height();
+               if(min<current)
+               {
+                   min=current;
+               }
+            });
+            min=parseInt(min)+15;
+            $(that).parent().css("min-height",min);
+        };
+                var current_slide=function (){
+                 var current=   $(that).attr("data-current");
+                 return current;
+                };
+                
+        var show=function (){
+            $(that).find(".item").fadeOut(1000);
+            var active=current_slide();
+            $(that).find("."+active).delay(1100).fadeIn();
+        };
+        var next=function (){
+            var current=current_slide();
+           var next=current.toString().replace("slide-","");
+           if(next>=total_slide)
+           {
+               $(that).attr("data-current","slide-0");
+               show();
+           }
+           else
+           {
+               next=parseInt(next)+1;
+               next="slide-"+next;
+               $(that).attr("data-current",next);
+               show();
+               
+           }
+        };
+        
+        init();
+        min_height();
+        show();
+        $(".next").click(function (){
+           next(); 
+        });
+        $(".prev").click(function (){
+           prev(); 
+        });
     };
-    $(".slider").abc();
-
-
+    $(".slider").picksSilder();
 });
